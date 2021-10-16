602bb27e166dd7c457057c932d6b5ef69c29f328
Stanislav Melekhin <darkprado@yandex.ru>
2021-10-16 23:09:04 +0300

    create project

---
+++ src/main/java/com/example/spring_boot_gutter/SpringBootGutterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_gutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGutterApplication.class, args);
+    }
+
+}
